2f22fa4b60c0ad6466c64e7aeb39498579831a32
Connor McCormack <mccormack.connor.m@gmail.com>
2020-03-24 15:49:55 -0500

    before adding their solution

--- server.js
+++ server.js
@@ -1,15 +1,39 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const POKEDEX = require('./pokedex.json')
+
+console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'))
 
-app.use((req, res) => {
-  res.send('Hello, world!')
-})
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+    console.log('validate bearer token middleware')
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+      return res.status(401).json({ error: 'Unauthorized request' })
+    }    // move to the next middleware
+    next()
+  })
 
-const PORT = 8000
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+function handleGetTypes(req, res) {
+  res.json(validTypes)
+}
+
+app.get('/types', handleGetTypes)
+
+function handleGetPokemon(req, res) {
+  res.send('Hello Pokemon!')
+}
+
+app.get('/pokemon', handleGetPokemon)
+
+const PORT = 8000;
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
